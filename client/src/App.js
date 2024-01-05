5b17a2ebf5cfdd9127942620b86f2c0e7be93581
Clayton Prickett <clayton.a.prickett@gmail.com>
2024-01-04 20:07:30 -0400

    commit

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+		<div className='App'>
+			<header className='App_header'>
+				<nav className='nav'>
+					<ul className='nav_list'>
+						<li className='nav_list_item'>HOME</li>
+						<li className='nav_list_item'>COMMUNITIES</li>
+						<li className='nav_list_item'>VENUE</li>
+						<li className='nav_list_item'>NOTIFICATIONS</li>
+					</ul>
+				</nav>
+			</header>
+			<main className='App_container'></main>
+			<footer className='App_footer'></footer>
+		</div>
+	);
+}
+
+export default App;
